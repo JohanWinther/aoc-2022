47481fe001f8a08c59afd84404848be260088243
Johan Winther <johan.winther@outlook.com>
2022-12-09 09:12:09 +0100

    Add template file for solutions

---
+++ template.hs
@@ -0,0 +1,15 @@
+#!/usr/bin/env runhaskell
+
+main :: IO ()
+main = do
+  input <- readFile "input.txt"
+  print $ part1 input
+  print $ part2 input
+
+part1 :: String -> Int
+part1 _ =
+  0
+
+part2 :: String -> Int
+part2 _ =
+  0
